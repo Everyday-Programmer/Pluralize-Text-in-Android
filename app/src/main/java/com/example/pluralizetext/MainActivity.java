4b2432afa5d2f2c87c279fe712e0c94eda52c2e5
Everyday Programmer <79357167+Everyday-Programmer@users.noreply.github.com>
2023-11-09 11:13:05 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/pluralizetext/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.pluralizetext;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSeekBar;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AppCompatSeekBar seekBar = findViewById(R.id.seekbar);
+        TextView textView = findViewById(R.id.textView);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (b) {
+                    textView.setText(getResources().getQuantityString(R.plurals.pluralize, i, i));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
